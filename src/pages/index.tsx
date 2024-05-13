772319818c4bafde1282e12c7a8a2806000fb727
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-14 02:26:15 +0900

    404에러 fix위한 store_data.json 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import Map from "@/components/Map";
 import Markers from "@/components/Markers";
 import StoreBox from "@/components/StoreBox";
 
-import * as stores from "@/data/store_data.json";
+import storeData from "@/data/store_data.json";
 
 export default function Home() {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
-  const storeDatas = stores["DATA"];
+  const storeDatas = storeData["DATA"];
 
   return (
     <>
