f7c16e5f66517544a14f05e3395652fb35c6ad9d
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-29 01:53:48 +0900

    검색 및 정렬 구현

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -2,19 +2,30 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreApiResponse, StoreType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 
+interface ResponseType {
+  page?: string;
+  limit?: string;
+  q?: string;
+  district?: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType>
 ) {
   const prisma = new PrismaClient();
 
-  const { page = "" }: { page?: string } = req.query;
+  const { page = "", limit = "", q, district }: ResponseType = req.query;
 
   if (page) {
     const skipPage = parseInt(page) - 1;
     const count = await prisma.store.count();
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
+      where: {
+        name: q ? { contains: q } : {},
+        address: district ? { contains: district } : {},
+      },
       take: 10,
       skip: skipPage * 10,
     });
