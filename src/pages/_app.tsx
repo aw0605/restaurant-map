3f64b169def638b3859f7beffd9b2c33a2f774ce
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-05-31 03:23:40 +0900

    맛집 등록 페이지 구현
    - react-hook-form을 이용한 등록 form생성
    - daum 주소 api를 이용한 위도, 경도 가져오기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
 import Layout from "@/components/Layout";
 
 import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <SessionProvider session={session}>
           <Layout>
             <Component {...pageProps} />
+            <ToastContainer />
           </Layout>
           <ReactQueryDevtools />
         </SessionProvider>
